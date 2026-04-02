17acae252b0dfb3abe091f661cd2199d7b8d25cf
Denise <SEUEMAIL@gmail.com>
2026-04-02 17:06:36 -0300

    fix: teste rota disciplinas id

--- app/api/aluno/disciplinas/[id]/route.ts
+++ app/api/aluno/disciplinas/[id]/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    message: "rota funcionando",
+    message: "rota [id] funcionando",
   });
 }
