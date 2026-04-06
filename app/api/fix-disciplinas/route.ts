4521a337eb8a297b306d90b3144426c67a00c6d2
Denise <SEUEMAIL@gmail.com>
2026-04-06 16:43:39 -0300

    fix: api fix-disciplinas build error

--- app/api/fix-disciplinas/route.ts
+++ app/api/fix-disciplinas/route.ts
@@ -4,10 +4,21 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  await prisma.disciplina.updateMany({
-    where: { professorId: 1 },
-    data: { professorId: 2 },
-  });
+  try {
+    await prisma.disciplina.updateMany({
+      where: { professorId: 1 },
+      data: { professorId: 2 },
+    });
 
-  return NextResponse.json({ message: "Disciplinas corrigidas!" });
+    return NextResponse.json({
+      message: "Disciplinas corrigidas!",
+    });
+  } catch (error) {
+    console.error("Erro fix-disciplinas:", error);
+
+    return NextResponse.json(
+      { error: "Erro ao corrigir disciplinas" },
+      { status: 500 }
+    );
+  }
 }
