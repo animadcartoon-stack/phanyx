ac592eda40c86f07e5fea2975e741b2b78b29662
Denise <SEUEMAIL@gmail.com>
2026-04-23 16:24:48 -0300

    acrescenta polos de instituições 01

--- app/admin/configuracoes/instituicao/page.tsx
+++ app/admin/configuracoes/instituicao/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+
 
 type LayoutProfissional =
   | "PHANYX_MODERNO"
