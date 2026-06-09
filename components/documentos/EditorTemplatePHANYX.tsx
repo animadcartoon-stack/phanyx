2126a2851e92cfc571d33caa434f6751ca486f4d
Denise <SEUEMAIL@gmail.com>
2026-06-09 13:23:09 -0300

    corrige import TextStyle do editor de templates

--- components/documentos/EditorTemplatePHANYX.tsx
+++ components/documentos/EditorTemplatePHANYX.tsx
@@ -5,7 +5,7 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TextAlign from "@tiptap/extension-text-align";
 import Underline from "@tiptap/extension-underline";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 
 type Props = {
