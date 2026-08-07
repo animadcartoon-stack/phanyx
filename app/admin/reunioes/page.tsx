784db28f03f69d0f527cbca53800ab5ee10486e7
Denise <SEUEMAIL@gmail.com>
2026-08-07 09:56:18 -0300

    inicia setor comercial completo

--- app/admin/reunioes/page.tsx
+++ app/admin/reunioes/page.tsx
@@ -667,23 +667,26 @@ export default function AdminReunioesPage() {
                         excluindoId === reuniao.id
                       }
                       className="
-      rounded-xl
-      border
-      border-red-300
-      bg-red-50
-      px-4
-      py-2
-      text-sm
-      font-semibold
-      text-red-700
-      hover:bg-red-100
-      disabled:cursor-not-allowed
-      disabled:opacity-50
-      dark:border-red-800
-      dark:bg-red-950/40
-      dark:text-red-200
-      dark:hover:bg-red-950/70
-    "
+  rounded-xl
+  border
+  border-red-700
+  bg-red-600
+  px-4
+  py-2
+  text-sm
+  font-semibold
+  text-white
+  shadow-sm
+  hover:bg-red-700
+  hover:border-red-800
+  disabled:cursor-not-allowed
+  disabled:opacity-50
+
+  dark:border-red-600
+  dark:bg-red-700
+  dark:text-white
+  dark:hover:bg-red-800
+"
                     >
                       Excluir
                     </button>
