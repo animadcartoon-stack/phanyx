700a3156bfdad13d6abab698e40aad3cb51f0f3a
Denise <SEUEMAIL@gmail.com>
2026-08-24 17:17:24 -0300

    inicia biblioteca virtual

--- app/aluno/trabalhos/[atividadeId]/entrega/EntregaTrabalhoAlunoClient.tsx
+++ app/aluno/trabalhos/[atividadeId]/entrega/EntregaTrabalhoAlunoClient.tsx
@@ -255,7 +255,7 @@ export default function EntregaTrabalhoAlunoClient({
   }
 
   return (
-    <div className="mx-auto max-w-6xl p-6">
+    <div className="phanyx-entrega-trabalho mx-auto max-w-6xl p-6">
       <div className="space-y-6 rounded-3xl border border-slate-200 bg-white p-6 shadow-sm dark:border-slate-800 dark:bg-slate-900">
         <div>
           <a
