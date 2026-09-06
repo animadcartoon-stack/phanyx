bd1cc7ed31e8d74923e091f9be354c5be88c48f6
Denise <SEUEMAIL@gmail.com>
2026-09-06 18:48:27 -0300

    otimiza build da vercel para reduzir uso de memoria

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import createNextIntlPlugin from "next-intl/plugin";
+﻿import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin(
   "./i18n/request.ts"
@@ -7,6 +7,14 @@ const withNextIntl = createNextIntlPlugin(
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 
   experimental: {
     serverComponentsExternalPackages: [
